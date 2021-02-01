b6dbb99fb9c0744a1967c6943fdea26236049574
Mohammed Alama <mohammedalsayed9688@gmail.com>
2021-02-01 15:46:44 +0200

    add missing lang

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -14,7 +14,8 @@ return [
     */
 
     'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-
+    'logout' => 'Logged Out Successfully',
+    'account_created' => 'Congratulations, Your Account is Created Successfully',
+    'successful' => 'Welcome Back :user',
 ];
